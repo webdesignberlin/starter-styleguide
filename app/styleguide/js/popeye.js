a51adb417135aea084ef6eafea9d7234922315dd
Michael Gerstmann <gerstmann@mcfit.com>
2016-04-01 16:38:42 +0200

    cleanup, bugfixing scrollspy

---
+++ app/styleguide/js/popeye.js
@@ -0,0 +1,77 @@
+/**
+ * POPEYE
+ *
+ * automatically generates anchored headings and nested navigation for h1 and h2
+ *
+ * usage:
+ * var options = {
+ *   headlineClass: String,     //class for the headlines
+ *   navigationClass: String,   //class of the navigation
+ *   anchor: String             //anchor text next to headlines
+ * };
+ * popeye(options);
+ *
+ */
+
+'use strict';
+
+function popeye(options) {
+
+  if (!options) {
+    options = {
+      headlineClass: 'headline',
+      navigationClass: 'list',
+      anchor: '#'
+    };
+  }
+
+  // get all elements with headline class
+  var headlines = document.getElementsByClassName(options.headlineClass);
+  var list = document.getElementsByClassName(options.navigationClass)[0];
+  var content = [];
+
+  for (var i = 0; i < headlines.length;  i++) {
+
+    // read the headline text and put it in content array
+    content[i] = headlines[i].innerText;
+
+    // get the headline, replace whitespace with dash and transform to lowercase for id
+    var headlineID = content[i];
+    headlineID = headlineID.replace(/\s+/g, '-').toLowerCase();
+    // add id to each headline element
+    headlines[i].id = headlineID;
+
+    //create links with id and add to the headlines
+    var a = document.createElement('a');
+    a.href = '#' + headlineID;
+    a.innerHTML = options.anchor;
+    headlines[i].appendChild(a);
+
+    // create the li elements that contain the links
+    var li = document.createElement('li');
+    var ali = document.createElement('a');
+    ali.href = '#' + headlineID;
+    ali.innerHTML = content[i];
+    li.appendChild(ali);
+
+    if (headlines[i].localName === 'h1') {
+      list.appendChild(li);
+    } else {
+      var nodelengeth = list.children.length - 1;
+
+      if(list.children[nodelengeth].children.length === 1) {
+        // first h2 after a h1 -> a new ul element needs to be created
+        var ul = document.createElement('ul');
+        ul.appendChild(li);
+        // get the position of the last create h1 li to add new list for h2
+        list.children[nodelengeth].appendChild(ul);
+      } else if(list.children[nodelengeth].children.length !== 1) {
+        // means that already a h2 exists and no ul needs to be created -> append to last ul li
+        var childLength = list.children[nodelengeth].children.length - 1;
+        list.children[nodelengeth].children[childLength].appendChild(li);
+      }
+    }
+
+  }
+
+}
--- app/styleguide/js/scrollSpy.js
+++ app/styleguide/js/scrollSpy.js
@@ -1,81 +1,23 @@
 module.exports = function(spyElement, spyWrapper){
 
-/*
-
-  //'use strict';
-
-  //var spyElement = spyElement;
-
-  if (spyElement){
-    spyElement = document.querySelectorAll(spyElement);
-  } else {
-    spyElement = document.querySelectorAll('.scrollspy');
-  }
-
-  if (spyWrapper){
-    spyWrapper = document.querySelector(spyWrapper);
-  } else {
-    spyWrapper = window;
-  }
-
-
-
-  var spyElements = {};
-  var i = 0;
-
-  Array.prototype.forEach.call(spyElement, function(e) {
-    spyElements[e.id] = e.offsetTop;
-    //spyElements[e.id] = e.getBoundingClientRect().top;
-  });
-
-  spyWrapper.addEventListener("scroll", updatePosition, false);
-  spyWrapper.addEventListener("resize", updatePosition, false);
-
-  function isScrolledIntoView(el) {
-    var elemTop = el.getBoundingClientRect().top;
-    var elemBottom = el.getBoundingClientRect().bottom;
-
-    var isVisible = (elemTop >= 0) && (elemBottom <= spyWrapper.scrollHeight);
-    return isVisible;
-  }
-
-  function updatePosition() {
-
-    var scrollPosition;
-
-    if(spyWrapper != window){
-      scrollPosition = this.scrollTop;
-    } else {
-      scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
-    }
-
-    for (i in spyElements) {
-
-      //var blubb = document.getElementById(i);
-      //console.log('offsetTop: ' + blubb + blubb.offsetTop);
-      //console.log('getBoundingClientRect().top: ' + blubb + blubb.getBoundingClientRect().top);
-
-
-      //console.log('scrollPosition: ' + scrollPosition);
-      //if (spyElements[i] <= scrollPosition) {
-
-      var elementCurrent = document.getElementById(i);
-      isScrolledIntoView(elementCurrent);
-
-      if (scrollPosition >= spyElements[i]) {
-        document.querySelector('.is-active').setAttribute('class', ' ');
-        document.querySelector('a[href*=' + i + ']').setAttribute('class', 'is-active');
-      }
-    }
-  }*/
-
-  spyWrapper = document.querySelector(spyWrapper);
-
   if('querySelector' in document && Array.prototype.forEach){
 
-    NodeList.prototype.forEach = Array.prototype.forEach;
-    spyWrapper.onscroll = function(){
+    if (!spyElement){
+      spyElement = '.scrollspy';
+    }
 
+    if (spyWrapper){
+      spyWrapper = document.querySelector(spyWrapper);
+    } else {
+      spyWrapper = window;
+    }
+
+    spyWrapper.addEventListener("scroll", updatePosition, false);
+    window.addEventListener("resize", updatePosition, false);
+
+    NodeList.prototype.forEach = Array.prototype.forEach;
+
+    function updatePosition(){
       spyElems = document.querySelectorAll(spyElement);
 
       spyElems.forEach(function(elem){
@@ -87,8 +29,6 @@ module.exports = function(spyElement, spyWrapper){
 
           linkSelector =  "a[href='#" + elem.getAttribute('id') + "']";
           nowActiveLink = document.querySelector(linkSelector);
-
-          console.log(linkSelector);
 
           if(nowActiveLink != null){
             curActiveLink  = document.querySelector('a.is-active');
@@ -104,6 +44,5 @@ module.exports = function(spyElement, spyWrapper){
 
     }
   }
-
 
 };
