7cee003773e16966c195c56744e92b1cc31bbf00
Michael Gerstmann <gerstmann@mcfit.com>
2016-04-04 14:25:04 +0200

    add robots

--- grunt/copy.js
+++ grunt/copy.js
@@ -5,34 +5,9 @@
  */
 module.exports = {
 
-    jquery: {
+    projectFiles: {
         expand: true,
-        src: '<%=globalConfig.bower%>/jquery/dist/jquery.min.js',
-        dest:'<%=globalConfig.dist%>/js/vendor/jquery',
-        flatten: true
-    },
-    fonts: {
-        expand: true,
-        src: '<%=globalConfig.app%>/fonts/*',
-        dest:'<%=globalConfig.dist%>/fonts/',
-        flatten: true
-    },
-    studium: {
-        expand: true,
-        cwd: '<%=globalConfig.app%>/studium/',
-        src: '**',
-        dest:'<%=globalConfig.dist%>/studium/',
-        flatten: false
-    },
-    favicon: {
-        expand: true,
-        src: '<%=globalConfig.app%>/*.ico',
-        dest:'<%=globalConfig.dist%>/',
-        flatten: true
-    },
-    json: {
-        expand: true,
-        src: '<%=globalConfig.app%>/*.json',
+        src: ['<%=globalConfig.app%>/*.ico', '<%=globalConfig.app%>/robots.txt'],
         dest:'<%=globalConfig.dist%>/',
         flatten: true
     }
