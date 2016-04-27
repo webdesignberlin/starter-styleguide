44ea2e06ed221eea76cf4d4691ae56b6fd81cbaa
Frank Holder <holder@mcfit.com>
2016-04-27 15:21:04 +0200

    #1 remove release config objects

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,34 +17,8 @@ module.exports = function (grunt){
 	  /* = Global Project Vars */
 
 
-		/**
-		 * + Global ssh variables for release
-		 * Usage: <%= sshconfig.var %>
-		 * =====================================================================
-		 */
-		var sshconfig = {
-			// local json file with login data for live
-			'live-login': grunt.file.readJSON('./release_data/live_host.json'),
-			// folder name of the webapp for live
-			'liveappfolder': '',
-			// root path of the project for live
-			'livehomepath': '/var/www/',
-			// path to the release directory for live
-			'liveapppath': '<%=sshconfig.livehomepath %><%=sshconfig.liveappfolder %>',
-			// Backup Archive Name
-			'backupName': 'starter-styleguide.tar'
-		};
-
-		var liveoptions = {
-			config: 'live-login',
-			// local stored ssh key for login on server
-			privateKey: grunt.file.read('./release_data/id_rsa')
-		};
-
 		grunt.file.defaultEncoding = 'utf8';
 		grunt.file.preserveBOM = false;
-
-		//grunt.file.write(filepath, contents [, options])
 	  
 	  require('load-grunt-config')(grunt, {
 	        // ...
@@ -53,21 +27,15 @@ module.exports = function (grunt){
 	        	jitGrunt: true,
 						staticMappings: {
 							'nunjucks': 'grunt-nunjucks-2-html',
-							'nunjucksDynamic': 'grunt-nunjucks',
-							sshconfig    : sshconfig,
-							liveoptions  : liveoptions,
-							sshexec: 'grunt-ssh',
-							sftp: 'grunt-ssh'
+							'nunjucksDynamic': 'grunt-nunjucks'
 						}
 	        },
 	  
 	  		data: {
 		      pkg          : grunt.file.readJSON('package.json'),
 		      globalConfig : globalConfig,
-					global       : grunt.file.readJSON(globalConfig.app + '/global.json'),
-					sshconfig    : sshconfig,
-					liveoptions  : liveoptions
+					global       : grunt.file.readJSON(globalConfig.app + '/global.json')
 	  		}
-	    });
+	  });
 
 };
